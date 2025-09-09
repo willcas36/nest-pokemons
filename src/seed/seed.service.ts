aa8d21681f7e30d9c51457e7bb1267b97f164212
William Quinones <william.quinones@believesol.com>
2025-09-09 14:05:48 -0400

    Antes de insertar por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { AxiosInstance, AxiosInterceptorManager } from 'axios';
+import axios from 'axios';
+import { PoleResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed(){
-    return 'Executed';
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    const { data } = await this.axios.get<PoleResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=1',
+    );
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+    return data.results;
   }
 }
